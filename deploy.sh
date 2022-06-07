1282807dc997072bceb87ca52bb034bc043bf13e
root <keithohno@gmail.com>
2022-06-07 11:51:13 -0400

    updated deployment

--- deploy.sh
+++
@@ -1,4 +0,0 @@
-if [ "$#" -ne 0 ]; then
-    echo $1 > hosts
-fi
-ansible-playbook deploy.yml
--- init.sh
+++
@@ -1,5 +0,0 @@
-rm ~/.ssh/known_hosts
-if [ "$#" -ne 0 ]; then
-    echo $1 > hosts
-fi
-ansible-playbook init.yml
